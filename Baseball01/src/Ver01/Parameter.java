5b7c760b33a7f86458e7722bbe96675155e056e2
HyeinChoi <hi.choi1031@gmail.com>
2020-10-30 16:33:58 +0900

    변수 인스턴스를 클래스로 변경(클래스 다이어그램 대체)

--- Baseball01/src/Ver01/Parameter.java
+++ Baseball01/src/Ver01/Parameter.java
@@ -1,18 +1,18 @@
 package Ver01;
 
-public interface Parameter {
+public class Parameter {
+	// 클래스 다이어그램 대체 : 이 안에 있는 변수 이용해 주세요
 	
-	String ID();      // 사용자 id
-	int PASS();       // 사용자 비번
-	int POINT();      // 포인트
-	int ADMINPASS();  // 관리자 비번
-	int CHARGE();     // 사용자가 입력하는 충전할 금액
-	int MYMONEY();    // 충전된 금액
-	int GRADE();      // 좌석등급n
-	int DATE();       // 경기일
-	int SEAT();       // 좌석
-	int MYSEAT();     // 내가 고른 좌석
-	int PRICE();      // 티켓 가격
+	String id;      // 사용자 id
+	int pass;       // 사용자 비번
+	int point;      // 포인트
+	int charge;     // 사용자가 입력하는 충전할 금액1
+	int myMoney;    // 충전된 금액
+	int Grade;      // 좌석등급n
+	int Date;       // 경기일
+	int Seat;       // 좌석
+	int mySeat;     // 내가 고른 좌석
+	int price;      // 티켓 가격
 
 
 
---
+++ Baseball01/src/Ver01/SaveReserve.java
@@ -0,0 +1,13 @@
+package Ver01;
+
+public class SaveReserve {
+     // 날짜, 좌석 선택 후 저장하는 메서드 - 혜인
+	
+	
+	// 날짜 입력받기
+	
+	
+	
+	
+	
+}
