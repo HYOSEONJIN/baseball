d7f93f0e7d506c89f483da0e8b6ac56a00625e01
jechoiiii <71616776+jechoiiii@users.noreply.github.com>
2020-10-30 18:03:50 +0900

    update interface & class

--- Baseball01/src/Ver01/UserInfo.java
+++ Baseball01/src/Ver01/UserInfo.java
@@ -1,24 +1,29 @@
 package Ver01;
 
-public class UserInfo {
+public class UserInfo implements LoginInfo{
 
-	// 기본 회원정보 
+	// 사용자 로그인 정보 
 
 	private String id;      	// 사용자 아이디
 	private int pw;       		// 사용자 비밀번호
-	private int myMoney = 0;    // 충전된 금액 
-	private int point = 0;      // 보유 포인트
+
+	public UserInfo(String id, int pw) {
+		this.id = id;
+		this.pw = pw;
+	}
+	
+
+	public String getId() {
+		return id;
+	}
+	public int getPw() {
+		return pw;
+	}
 
 
-	// 정보 출력 기능
-	void showUserInfo() {
-		System.out.println(id +"님의 회원 정보입니다.");
-		System.out.println("=====================");
+	// 정보 출력 
+	void showLoginInfo() {
 		System.out.println("아이디 : "+ id);
-		System.out.println("비밀번호 : "+ pw);
-		System.out.println("충전 금액: "+ myMoney);
-		System.out.println("보유 포인트 : "+ point);
-		System.out.println("=====================");
-		
+		System.out.println("비밀번호 : "+ pw);	
 	}
 }
--- Baseball01/src/Ver01/UserInfoManager.java
+++ Baseball01/src/Ver01/UserInfoManager.java
@@ -1,32 +1,33 @@
 package Ver01;
 
+import java.util.List;
+
 public class UserInfoManager {
+	
+	// 로그인정보 배열 생성
+	List<UserInfo> userInfo;
 
 	
-	// 로그인 메서드
+	// 로그인 메서드 : 사용자 입력 (창 띄우기)
 	public void login(){
-		
-		// 
-		
 	}
 	
-	// 회원가입 : 정보 저장 메서드
-	public void addInfo() {
-		
+	// 정보 저장 메서드
+	public void addInfo() {			
+		// 아이디 중복 확인	
 	}
+
+	// 자가정보 조회 	
+	public void displayInfo(){
+		// id/pw 출력
+		// 보유 금액, 보유 포인트 출력	
+		// 예매이력 출력 
+		// 환불이력 		
+		// 이벤트 당첨 조회
+	}	
 	
-	// 자가정보 조회 
-	public void searchInfo(){
-		
-		// ID/PW, 보유 금액, 보유 포인트 출력
-		
-		
-		// 예매이력 출력 (추후 추가)
-		
-		// 환불이력 (추후 추가)
-		
-		// 이벤트 당첨 조회 (추후 추가)
-		
+	// 탈퇴 메서드
+	public void deleteInfo() { 
 	}
 	
 }
