9886dd5dc77b978fd7d0514f6a55dc6327dadd9c
JuRyang <wnfid1040@gmail.com>
2020-10-30 17:59:28 +0900

    회원관리

---
+++ Baseball01/src/Ver01/AdminManger.java
@@ -0,0 +1,54 @@
+package Ver01;
+
+public class AdminManger {
+
+	String id;
+//1.전체정보조회(회원가입 한 전체 조회)
+	public void showAllinfor() {
+		
+	}
+//2.회원검색(아이디로 조회하기)
+	public void insert() {
+		
+	}
+//3.결제현황리스트	
+	public void pay() {
+		
+	}
+	
+//4.이벤트정보조회 (win ,lose구분)
+	public void eventInfor() {
+		
+	}
+
+	
+	//5.회원탈퇴
+	//회원 id로 검색해서 검색하기 
+	public void deleteInfor() {
+		
+
+		//탈퇴 할 회원 검색 
+		System.out.println("회원탈퇴메뉴입니다.");
+		System.out.println("탈퇴 할 회원 아이디를 입력해주세요");
+		String id = Util.sc.nextLine();
+		
+		int index = -1; //정보가 없다는걸 알려주기 위한 값
+		
+		//검색 인덱스로 값는다.
+		for(int i = 0; i<10; i++) {
+			if()
+		}
+		
+		
+		
+	}
+	
+	
+
+	
+	
+
+
+	
+
+}
