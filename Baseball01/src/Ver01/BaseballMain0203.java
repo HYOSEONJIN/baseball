720d2a51475cca9fe8397fcca15b0529513a52b1
hyenni2-2 <hi.choi1031@gmail.com>
2020-11-02 15:48:25 +0900

    메인0203

---
+++ Baseball01/src/Ver01/BaseballMain0203.java
@@ -0,0 +1,76 @@
+package Ver01;
+
+import java.awt.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
+public class BaseballMain0203 implements Menu {
+
+   public static void main(String[] args) throws IOException {
+      // 예매 메인
+      
+      // 로그인,예매, 회원정보 조회를 위한 인스턴스 생성
+      UserInfoManager user = UserInfoManager.getInstance();
+      PointZoneManager pointZone = new PointZoneManager();
+      
+      // 메인 메뉴 만들기 - 메뉴 부분 인터페이스 차후 수정
+      int select;
+      while(true) {
+         System.out.println("********2020 포스트시즌 야구 예매********");
+         System.out.println("*             "+ LOGIN+".로그인                              *");
+         System.out.println("*              "+BOOKING+".예매                               *");
+         System.out.println("*             "+INFO+".회원정보                           *");
+         System.out.println("*        "+SAVE+".예매내역 외부 파일로 저장              *");
+         System.out.println("*             "+ EVENT+".이벤트                              *");
+         System.out.println("*           "+EXIT+".페이지 닫기                           *");
+         System.out.println("**********************************");
+         System.out.println("원하는 메뉴의 숫자를 입력하세요 >> ");
+         try {
+         select = Util.sc.nextInt();
+         if(select<0 || select>6) {
+            BadMenuException e = new BadMenuException(select);
+            throw e;
+         } 
+         } catch(BadMenuException | InputMismatchException e) {
+            System.out.println("잘못된 입력입니다. 다시 입력하세요.");
+            Util.sc.nextLine();
+            continue;
+         }
+         
+         
+         
+         switch(select) {
+         case LOGIN: 
+            user.addProfile();
+            break;
+//         case BOOKING:
+//            
+//            break;
+//         case INFO:
+//            
+//            break;
+         case SAVE:
+         //   SaveManager.save();
+            break;
+         case EVENT:
+            pointZone.pointZone();
+            break;
+         case EXIT:
+            System.out.println("예매창을 닫습니다.");
+            System.exit(0);
+         }
+         
+
+   }
+
+      
+      
+      
+      
+      
+      
+
+   }
+
+}
---
+++ Baseball01/src/Ver01/Menu.java
@@ -0,0 +1,11 @@
+package Ver01;
+
+public interface Menu {
+
+	int LOGIN=1;
+	int BOOKING=2;
+	int INFO=3;
+	int SAVE=4;
+	int EVENT=5;
+	int EXIT=6;
+}
