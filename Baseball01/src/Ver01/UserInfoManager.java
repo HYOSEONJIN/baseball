13dfbc0d7de26102d9732487ffe71fea484f7a3b
JuRyang <wnfid1040@gmail.com>
2020-11-03 12:24:07 +0900

    예매이력 수정하기

--- Baseball01/src/Ver01/UserInfoManager.java
+++ Baseball01/src/Ver01/UserInfoManager.java
@@ -1,16 +1,24 @@
 package Ver01;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.List;
 
-public class UserInfoManager {
+
+
+
+public class UserInfoManager  extends LoginInfoManager{
 	
 
-	LoginInfoManager log = LoginInfoManager.getInstance(); 
+	//LoginInfoManager log = LoginInfoManager.getInstance();  
 
+	PointZoneManager pointZoneManager = new PointZoneManager(); //포인트
+	
+	
 	
 //	List<LoginInfo> loginInfo;
 //	LoginInfo info = new LoginInfo();
-	
+//	
 	
 	
 //	// 외부에서 참조변수 받을 수 있는 메서드
@@ -18,7 +26,7 @@ public class UserInfoManager {
 //		return manager;
 //	}
 	
-	  /*public void logMenu() {
+	/*  public void showMyInfo()  {
 	         
 	         while(true) {
 	         System.out.println("1.예매조회");
@@ -26,10 +34,10 @@ public class UserInfoManager {
 	         System.out.println("3.이벤트내역조회");
 	         System.out.println("4.탈퇴하기");
 	         System.out.println("5.나가기");
-	         int choice = Util.sc.nextInt();
+	         int select = Util.sc.nextInt();
 	         Util.sc.nextLine();
 	         
-	         switch(choice) {
+	         switch(select) {
 	         case 1:
 	            displayInfo(); //예매조회
 	            break;
@@ -52,6 +60,58 @@ public class UserInfoManager {
 	   
 
 	   // 자가정보 조회    ++++주량 
+	
+
+	public void showMyInfo(){
+	      if(NOWID==null) {
+	         System.out.println("로그인이 필요합니다");
+	         return;
+	      }
+
+	      while(true) {
+	      System.out.println("1. 예매조회");
+	      System.out.println("2. 이벤트조회");
+	      System.out.println("3. 회원탈퇴");
+	      
+	      System.out.println("\n>>");
+	      
+	      // 사용자 선택
+	      int select =0;
+	      
+	      try {
+	    	  
+	    	 select =Util.sc.nextInt();
+	 
+	            //  메뉴 외 입력 시 예외처리
+	            if( select!=1 && select!=5 ) {
+	            	 BadMenuException e = new   BadMenuException (select);
+	            throw e;   
+	            }
+	          
+	            switch(select) {
+		         case 1:
+		            displayInfo(); //예매조회
+		            break;
+		         case 2:
+		            showEvent();  //이벤트조회
+		            break;
+		         case 3:
+		            deleteInfo(); //회원탈퇴
+		            break;
+		         case 4:   //나가기
+		            return;
+	            }         
+	            
+	      }catch(InputMismatchException  | BadMenuException e) {
+				System.out.println("올바르지 않은 입력입니다.");
+				Util.sc.nextLine();
+				continue;
+			}
+	}
+	
+	      
+	}
+ 
 	   
 	   // searchInfo() 메서드로 자가정보에 해당하는 index 찾아서 해당 index 정보 출력
 	   // id/pw 출력
@@ -62,36 +122,71 @@ public class UserInfoManager {
 	//예매이력(보유 금액, 보유 포인트 출력)
 	public void displayInfo(){
 	      // id/pw 출력
-	      if( log.loginInfo.size() == 0) {
+	      if( loginInfo.size() == 0) {
 	         System.out.println("조회 할 정보가 없습니다.");
 	         return;
 	      }
 	      
 	      System.out.println("예매 확인 할 아이디 입력해주세요:");
 	      String id = Util.sc.nextLine();
-	      System.out.println("비밀번호 : ");
-	      int pass = Util.sc.nextInt();
+
 	      
 	      int index = -1;
 	      
-	      for(int i = 0; i<((List<LoginInfo>) log).size();i++) {
+	      for(int i = 0; i<loginInfo.size();i++) {
 	         // 입력된 아이디와 패스워드가 일치하는 정보를 찾음.
-	         if (log.loginInfo.get(i).getId().equals(id)
-	               && log.loginInfo.get(i).getPw().equals(pass)) {  
+	         if (loginInfo.get(i).getId().equals(NOWID)) {  
 	            index = i;
 	         } else {
-	            System.out.println(id + " 아이디는 존재하지 않음.");
+	            System.out.println(NOWID + " 아이디는 존재하지 않음.");
 	         }
 	      }
 	      
 	      if(index <0) {
-	         System.out.println("일치하는 아이디 "+id+"의 정보가 존재하지 않습니다.");
+	         System.out.println("일치하는 아이디 "+NOWID+"의 정보가 존재하지 않습니다.");
 	         System.out.println("메뉴로 이동합니다.");
 	      }else {
 	        System.out.println("예매 내역입니다.");
-	      //  log.loginInfo.get(index)).viewAll(); //예약 명단 전체 정보출력에서 입력한 인덱스 찾아 출력
-	      }
-	   }
+	     //   loginInfo.get(index).mySeatView() ; //현재 접속한 유저 값으로 설정하기??
+	      } 
+		
+		
+		/*
+		// id/pw 출력
+				if (loginInfo.size() > 0) {
+					for (int i = 0; i < loginInfo.size(); i++) {
+						// 입력된 tempUserId가 저장된 유저 정보에 존재하는지 찾음
+						if (loginInfo.get(i).getId().equals(id)) {
+							loginInfo = (List<LoginInfo>) loginInfo.get(i);
+						}
+					}
+
+					
+					if (loginInfo != null) {
+						System.out.println("id : " + loginInfo.getId());
+						System.out.println("pw : " + loginInfo.getPass());
+						// 보유 금액, 보유 포인트 출력
+						System.out.println("user money : " + loginInfo.getMyMoney());
+						System.out.println("user point : " + loginInfo.getPoint());
+						// 예매이력 출력
+						if(loginInfo.viewAll() != null) {
+							loginInfo.getBookInfo().printBookInfo();
+	         			}else {
+							System.out.println(id + "예매 이력 없음.");
+
+						}
+					
+					}else  {
+						System.out.println(tempUserId + "라는 아이디는 존재하지 않음.");
+					}
+				} else {
+					System.out.println("등록된 유저정보 없음");
+				}*/
+}
+		
+		
+		
+		
 	      
 	/*
 	//횐불이력
@@ -131,13 +226,13 @@ public class UserInfoManager {
 
 
 
-	PointZoneManager pointZoneManager = new PointZoneManager(); //포인트 클래스 
+	//PointZoneManager pointZoneManager = new PointZoneManager(); //포인트 클래스 
 
 	//이벤트내역 조회
 	public void showEvent() {
 	   
 	   // id/pw로 확인
-	   if(log.loginInfo.size() == 0) {
+	   if(loginInfo.size() == 0) {
 	      System.out.println("유저등록된 유저 정보가 없음.");
 	      return;
 	   }
@@ -148,11 +243,11 @@ public class UserInfoManager {
 	   
 	   String name = null;
 	   
-	   for(int i = 0; i<log.loginInfo.size(); i++) {
+	   for(int i = 0; i<loginInfo.size(); i++) {
 	      // 입력된 아이디와 패스워드가 일치하는 정보를 찾음.
 //	      if (log.get(i).getId().equals(id)) {    
 //	         index = i;
-	      if(log.loginInfo.get(i).getId().equals(id)){
+	      if(loginInfo.get(i).getId().equals(id)){
 	      } else {
 	         System.out.println("이벤트 내역이 존재하지 않음.");
 	      }
@@ -172,7 +267,7 @@ public class UserInfoManager {
 	   
 	// 회원탈퇴 메서드
 	public void deleteInfo() {
-	   if(log.loginInfo.size() == 0) {
+	   if(loginInfo.size() == 0) {
 	      System.out.println("유저등록된 유저 정보가 없음.");
 	      return;
 	   }
@@ -184,10 +279,10 @@ public class UserInfoManager {
 	   
 	   int index = -1;
 	   
-	   for(int i = 0; i<log.loginInfo.size();i++) {
+	   for(int i = 0; i<loginInfo.size();i++) {
 	      // 입력된 아이디와 패스워드가 일치하는 정보를 찾음.
-	      if (log.loginInfo.get(i).getId().equals(id)
-	            && log.loginInfo.get(i).getPw().equals(pass)) {  
+	      if (loginInfo.get(i).getId().equals(id)
+	            && loginInfo.get(i).getPw().equals(pass)) {  
 	         index = i;
 	      } else {
 	         System.out.println(id + "라는 아이디는 존재하지 않음.");
@@ -195,13 +290,24 @@ public class UserInfoManager {
 	   }
 	   
 	   if(index <0) {
-	      System.out.println("일치하는 아이디 "+id+"의 정보가 존재하지 않습니다.");
-	      System.out.println("메뉴로 이동합니다.");
-	   }else {
-		   log.loginInfo.remove(index);
-	      System.out.println("정상적으로 탈퇴되었습니다.");
-	   }
-	   
-	}
-
+	         System.out.println("일치하는 아이디 "+id+"의 정보가 존재하지 않습니다.");
+	         System.out.println("메뉴로 이동합니다.");
+	      }else {
+	         if(NOWID != null){
+	           NOWID = null;
+	         }
+	
+	         loginInfo.remove(index);
+	         System.out.println("정상적으로 탈퇴되었습니다.");
+	      } 
+	      
+	      if(NOWID== null) {
+	         System.out.println("로그아웃이 되었습니다.");
+	      }
+	
+	}   
 }
+	   
+	
+
+
