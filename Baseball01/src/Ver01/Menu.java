9e520fba99a0368b187d8b574f617e7bafcbdddd
HYOSEONJIN <hyoseonjin1@gmail.com>
2020-11-03 18:07:07 +0900

    Update Menu.java

--- Baseball01/src/Ver01/Menu.java
+++ Baseball01/src/Ver01/Menu.java
@@ -5,9 +5,8 @@ public interface Menu {
 	int LOGIN=1;
 	int BOOKING=2;
 	int INFO=3;
-	int SAVE=4;
-	int EVENT=5;
-	int EXIT=6;
+	int EVENT=4;
+	int EXIT=5;
 	
 	// *****************************
 	// 로그인 상세메뉴 (LoginInfoManager)
