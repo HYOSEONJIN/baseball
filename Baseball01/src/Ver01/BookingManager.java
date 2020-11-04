f7c3ce77a3af820f59616c2f900f725900d9c542
2hundred2ne2 <2hundred2ne2@gmail.com>
2020-11-04 14:59:22 +0900

    결제 충전 추가

--- Baseball01/src/Ver01/BookingManager.java
+++ Baseball01/src/Ver01/BookingManager.java
@@ -5,94 +5,75 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-
 public class BookingManager implements Menu {
-		
-	//public void Bookingmain() {
+
+	// public void Bookingmain() {
 	public static void main(String[] args) {
-		
+
 		ReserveSeat reserve = new ReserveSeat();
-		
+
 		// 테스트 변수
 		boolean result = false;
-				
+
 		// 테스트를 위해 우선 넣어놓음 . 테스트 끝나면 아래 주석 풀기
-		//String loginId = LoginInfoManager.NOWID;
+		// String loginId = LoginInfoManager.NOWID;
 		String loginId = "이수진";
 
-		String choiceDate = null; 	// 선택한 야구날짜
-		char grade = ' ';  		// 좌석 등급 
-		int price = 0;			// 티켓 가격
-		int myMoney=0;			//현재 가진 돈 
-		int point=0;			//포인트
-		
-		while(true) {
-			
+		String choiceDate = null; // 선택한 야구날짜
+		char grade = ' '; // 좌석 등급
+		int price = 0; // 티켓 가격
+		int myMoney = 5000; // 현재 가진 돈
+		int point = 0; // 포인트
+		int getPoint = myMoney / 10;
+		while (true) {
+
 			System.out.println("********2020 포스트시즌 야구 예매********");
-	         System.out.println("*             "+  BOOKINGNOW+".예매하기                     *");
-	         System.out.println("*              "+CANCEL+".예매취소                         *");
-	     	 System.out.println("*        "+BOOKINGINFO+".결제정보                      *");
-	         System.out.println("*           "+RECHARGE+".충전하기                           *");
-	         System.out.println("**********************************");
-			
+			System.out.println("*             " + BOOKINGNOW + ".예매하기                     *");
+			System.out.println("*              " + CANCEL + ".예매취소                         *");
+			System.out.println("*        " + BOOKINGINFO + ".결제정보                      *");
+			System.out.println("*           " + RECHARGE + ".충전하기                           *");
+			System.out.println("**********************************");
+			System.out.println("보유금액:" + myMoney);
 			int choice = Util.sc.nextInt();
 			Util.sc.nextLine();
-			
-			switch(choice) {
-				// 예약 하기
-				case BOOKINGNOW:		
-					reserve.insertSeat();
-					System.out.println("결제 하시겠습니까?");
-					System.out.println("1.예 ,2.아니요");
-					
-					break;
-					
-				// 예약 취소		
-				case CANCEL:
-					reserve.cancelSeat();
-					break;
-					
-				// 예약 정보보기		
-				case BOOKINGINFO:
-					//내 좌석 정보보기
-					reserve.mySeatView(); 
-					break;
+
+			switch (choice) {
+			// 예약 하기
+			case BOOKINGNOW:
+				reserve.insertSeat();
 				
-					//결제하기		
-			/*
-						
-					if (grade=='A') {
-						price = 10000;  
-						point = price/10;
-						System.out.println(price+"원이 결제 되었습니다."); 
-						System.out.println(point +"가  적립 되었습니다."); 
-	 				}else  if (grade=='B') {
-						price=5000;
-					    point=price/10;
-						System.out.println(price+"원이 결제 되었습니다."); 
-						System.out.println(point+ "가  적립 되었습니다."); 
-					}else  if (grade=='c') {
-						price=3000;
-						point=price/10;
-						System.out.println(price+"원이 결제 되었습니다."); 
-						System.out.println(point+"가  적립 되었습니다."); 
-					}
-*/					
-					
-					
-				case RECHARGE:
-/*					
-					System.out.println("충전 금액을 입력 하세요 ");
-					   
-				   int recharge=Util.sc.nextInt();
-				    myMoney=+ recharge; 
+				boolean sucess = (boolean)reserve.paying(myMoney);
+
+				// 결제가 제대로 완료된 경우 
+				if(sucess) {
+					reserve.payed();
+				} 		
 				
-				    System.out.println(myMoney+"가 충전되었습니다. ");
-*/					
-					break;
+				break;
+
+			case CANCEL:
+
+				reserve.cancelSeat();
+
+				break;
+
+			// 예약 정보보기
+			case BOOKINGINFO:
+				// 내 좌석 정보보기
+				reserve.mySeatView();
+				break;
+
+			// 결제하기
+
+			case RECHARGE:
 				
+			myMoney = reserve.recharge(myMoney);
+				
+				
+				break;
+
 			}
 		}
-	
+
 	}
 }
--- Baseball01/src/Ver01/ReserveSeat.java
+++ Baseball01/src/Ver01/ReserveSeat.java
@@ -7,169 +7,205 @@ import java.util.Date;
 import java.util.List;
 
 public class ReserveSeat {
-	static String choiceDate;  
-	static int seatNum = 0;   		// 좌석 번호
-	
-	//static String loginId = LoginInfoManager.NOWID;
+	static String choiceDate;
+	static int seatNum = 0; // 좌석 번호
+	char grade = ' '; // 좌석 등급
+	int price = 0; // 티켓 가격
+	int myMoney = 0; // 현재 가진 돈
+	int point = 0; // 포인트
+	int getPoint = myMoney / 10;// 충전 포인트
+	static int choiceSeatNum = 0;
+	int addMoney;
+
+	// static String loginId = LoginInfoManager.NOWID;
 	static String loginId = "이수진";
-	
+
 	// 배열을 이용해서 저장하는 방식을 ArrayList<T> 컬랙션을 이용해서 구현해 보자
 	static List<Seat> pSeat;
-	
-	// 생성자 : 싱글톤 처리 -> 외부에서 인스턴스 생성을 금지  
-	ReserveSeat(){
+
+	// 생성자 : 싱글톤 처리 -> 외부에서 인스턴스 생성을 금지
+	ReserveSeat() {
 		// List<PhoneInfor> 초기화
 		pSeat = new ArrayList<Seat>();
-		
+
 		// 생성자 호출할때 파일 불러오기
-		//read();
+		// read();
 	}
-	
-	// 날짜 입력하기 
+
+	// 날짜 입력하기
 	public static String choiceDate() {
 		getMonthGalendar();
-		
+
 		System.out.println("예매 가능한 날짜를 입력해 주세요. [입력형식 : 2020-01-01]");
-		
-		while(true) {
+
+		while (true) {
 			// 입력 날짜 받기
 			choiceDate = Util.sc.nextLine();
-			
+
 			if (choiceDate.length() != 10) {
 				System.out.println("입력형식이 올바르지 않습니다. 다시 입력해주세요.");
-				//Util.sc.nextLine();
+				// Util.sc.nextLine();
 				continue;
 			} else {
 				System.out.println("[" + choiceDate + "] 경기를 선택하셨습니다.");
 				break;
 			}
 		}
-		
+
 		return choiceDate;
 	}
-	
-	// 좌석 선택하기 
+
+	// 좌석 선택하기
 	public static int choiceSeat(String choiceDate) {
-		String arrSeat[][] = new String[3][10];  // 3 * 10 좌석 생성 
-		int choiceSeatNum = 0; 
+		String arrSeat[][] = new String[3][10]; // 3 * 10 좌석 생성
+		choiceSeatNum = 0;
 		int index = 0;
 		int count = 1;
-		
+
 		System.out.println("========== 좌석 정보 [예약된 좌석인 경우 X, 아닌경우 : 좌석번호] ==========");
-		for(int i = 0; i < arrSeat.length ; i++) {
-			
-			System.out.println("[" + (char)(i + 'A') + "석]");
-			
-			for(int j = 0; j < arrSeat[i].length ; j++) {
-				index = searchIndex(choiceDate, count); 
-				
+		for (int i = 0; i < arrSeat.length; i++) {
+
+			System.out.println("[" + (char) (i + 'A') + "석]");
+
+			for (int j = 0; j < arrSeat[i].length; j++) {
+				index = searchIndex(choiceDate, count);
+
 				// 예약된 좌석인 경우 X, 아닌경우 : 좌석번호
-				if(index > -1) {
+				if (index > -1) {
 					System.out.print("[X]");
-				}else {
+				} else {
 					System.out.print("[" + count + "]");
 				}
 				count++;
 			}
 			System.out.println();
 		}
-		System.out.println("=============================================");	
-		
+		System.out.println("=============================================");
+
 		System.out.println("원하시는 좌석 번호를 선택해주세요.");
 		choiceSeatNum = Util.sc.nextInt();
-		
-		return choiceSeatNum; 
+
+		return choiceSeatNum;
 	}
-	
-	// 좌석 예약하기 
+
+	// 좌석 예약하기
 	public static void insertSeat() {
-		// 선택한 야구 날짜 
+		// 선택한 야구 날짜
 		choiceDate = choiceDate();
-		
-		// 선택한 좌석번호 
+
+		// 선택한 좌석번호
 		seatNum = choiceSeat(choiceDate);
-		
-		// 예약
-		pSeat.add(new Seat(loginId, choiceDate, seatNum));
-		System.out.println(loginId + "님 날짜 : " + choiceDate + ",  좌석번호 : " + seatNum + "번 예매 되셨습니다");		
+
 	}
-	
+
+	// 결제
+	boolean paying(int myMoney) {
+		boolean result = false;
+
+		if (seatNum <= 10) {
+			price = 10000;
+		} else if (seatNum > 10 && seatNum <= 20) {
+			price = 5000;
+		} else if (seatNum > 20 && seatNum <= 30) {
+			price = 3000;
+		}
+
+		if (myMoney < price) {
+			System.out.println("금액 충전이 필요합니다.");
+		} else {
+			myMoney -= price;
+			point = price / 10;
+			System.out.println(price + "원이 결제 되었습니다.");
+			System.out.println(point + "가  적립 되었습니다.");
+			result = true;
+		}
+
+		return result;
+	}
+
+	// 예약완료
+	public static void payed() {
+		pSeat.add(new Seat(loginId, choiceDate, seatNum));
+		// System.out.println(pSeat.size());
+		System.out.println(loginId + "님 날짜 : " + choiceDate + ",  좌석번호 : " + seatNum + "번 예매 되셨습니다");
+	}
+
 	// 좌석 취소하기
 	public static void cancelSeat() {
-		//내 좌석 정보보기
-		mySeatView(); 
-		
+		// 내 좌석 정보보기
+		mySeatView();
+
 		System.out.println("취소하시는 번호를 입력해주세요.");
 		int index = Util.sc.nextInt();
-		
-		if(index < 1) {
+
+		if (index < 1) {
 			System.out.println("찾으시는 정보가 존재하지 않습니다.");
 			System.out.println("메뉴로 이동합니다.");
 		} else {
-			// 좌석 취소 
+			// 좌석 취소
 			pSeat.get(index - 1).cancel();
 			System.out.println(loginId + "님 예약취소가 완료되었습니다.\n");
 		}
 	}
-	
+
 	// 해당달 달력 가져 오기
-	public static void getMonthGalendar(){
+	public static void getMonthGalendar() {
 
 		// 달력 관련 변수
-		Calendar cal = Calendar.getInstance(); 
-		int thisYear = cal.get(Calendar.YEAR);  		// 년
-		int thisMonth = cal.get(Calendar.MONTH)+1; 		// 월
-		int today = cal.get(Calendar.DATE); 			// 일
-		
-        cal.set(thisYear, thisMonth - 1, 1); 			// thisMonth - 1 : 1월이 0이라서 해당달을 set 하려면 -1 해야함 
-       
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); 		// 1일의 요일
-        int endDate = cal.getActualMaximum(Calendar.DATE); 	// 달의 마지막일
-       
-        int nowYear = cal.get(Calendar.YEAR);
-        int nowMonth = cal.get(Calendar.MONTH);
-       
-        System.out.println("[" + nowYear + "년  " + (nowMonth + 1) + "월]");
+		Calendar cal = Calendar.getInstance();
+		int thisYear = cal.get(Calendar.YEAR); // 년
+		int thisMonth = cal.get(Calendar.MONTH) + 1; // 월
+		int today = cal.get(Calendar.DATE); // 일
 
-        //요일명 출력
-        System.out.println(" 일\t 월\t 화\t 수\t 목\t 금\t 토\t");  
-        System.out.println("====================================================");
-        
-        //1일이 시작되는 이전의 요일 공백으로 채우기
-        int dateNum = 1; //출력할 date를 저장할 변수
-      
-        for (int i = 1; dateNum <= endDate ; i++) {    
-           
-            if(i < dayOfWeek) {
-            	System.out.print("\t"); // 요일숫자보다 작으면 공백
-            } else {
-                if(dateNum < today) {
-                	System.out.print("\t"); 	//오늘 날짜전이면 예약 불가능 하므로 공백
-                } else {
-                	System.out.print(dateNum + "\t"); 		
-                }
-               
-                dateNum++; 
-            }      
-            
-            // 줄바꿈 
-            if(i % 7 == 0) {
-            	System.out.println(); 
-            }
-           
-        }
-        System.out.println();
-    }
+		cal.set(thisYear, thisMonth - 1, 1); // thisMonth - 1 : 1월이 0이라서 해당달을 set 하려면 -1 해야함
 
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); // 1일의 요일
+		int endDate = cal.getActualMaximum(Calendar.DATE); // 달의 마지막일
+
+		int nowYear = cal.get(Calendar.YEAR);
+		int nowMonth = cal.get(Calendar.MONTH);
+
+		System.out.println("[" + nowYear + "년  " + (nowMonth + 1) + "월]");
+
+		// 요일명 출력
+		System.out.println(" 일\t 월\t 화\t 수\t 목\t 금\t 토\t");
+		System.out.println("====================================================");
+
+		// 1일이 시작되는 이전의 요일 공백으로 채우기
+		int dateNum = 1; // 출력할 date를 저장할 변수
+
+		for (int i = 1; dateNum <= endDate; i++) {
+
+			if (i < dayOfWeek) {
+				System.out.print("\t"); // 요일숫자보다 작으면 공백
+			} else {
+				if (dateNum < today) {
+					System.out.print("\t"); // 오늘 날짜전이면 예약 불가능 하므로 공백
+				} else {
+					System.out.print(dateNum + "\t");
+				}
+
+				dateNum++;
+			}
+
+			// 줄바꿈
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+
+		}
+		System.out.println();
+	}
 
 	// 정보 검색 1
 	// 해당 indxe의 참조변수로 정보 출력
 	// 배열의 index 를 찾는 메서드
 	public static int searchIndex(String name, String date, int seatNum) {
 		int index = -1; // 정보가 없을때
-		
-		for(int i=0; i < pSeat.size() ; i++) {
-			if(pSeat.get(i).getName().equals(name) && pSeat.get(i).getDate().equals(date) && pSeat.get(i).getSeatNum() == seatNum) {
+
+		for (int i = 0; i < pSeat.size(); i++) {
+			if (pSeat.get(i).getName().equals(name) && pSeat.get(i).getDate().equals(date)
+					&& pSeat.get(i).getSeatNum() == seatNum) {
 				index = i;
 			}
 		}
@@ -181,27 +217,35 @@ public class ReserveSeat {
 	// 배열의 index 를 찾는 메서드
 	public static int searchIndex(String date, int seatNum) {
 		int index = -1; // 정보가 없을때
-		
-		for(int i=0; i < pSeat.size() ; i++) {
-			if(pSeat.get(i).getDate().equals(date) && pSeat.get(i).getSeatNum() == seatNum) {
+
+		for (int i = 0; i < pSeat.size(); i++) {
+			if (pSeat.get(i).getDate().equals(date) && pSeat.get(i).getSeatNum() == seatNum) {
 				index = i;
 			}
 		}
 		return index;
 	}
-	
-	// 내 좌석 정보 보기 
+
+	// 내 좌석 정보 보기
 	public static void mySeatView() {
-		String result =  "예약된 정보가 존재하지 않습니다.";
-		
-		System.out.println("["+ loginId + "님 예약 정보]");
-		for(int i=0; i < pSeat.size() ; i++) {
-			if(pSeat.get(i).getName().equals(loginId)) {
-				result = (i + 1) + ". [" + pSeat.get(i).getName() + "]님은 " + pSeat.get(i).getDate() + "일 " + pSeat.get(i).getGrade() + "등급 " + pSeat.get(i).getSeatNum() + "번째 좌석을 예약하셨습니다.";
-			}			
+		String result = "예약된 정보가 존재하지 않습니다.";
+
+		System.out.println("[" + loginId + "님 예약 정보]");
+		for (int i = 0; i < pSeat.size(); i++) {
+			if (pSeat.get(i).getName().equals(loginId)) {
+				result = (i + 1) + ". [" + pSeat.get(i).getName() + "]님은 " + pSeat.get(i).getDate() + "일 "
+						+ pSeat.get(i).getGrade() + "등급 " + pSeat.get(i).getSeatNum() + "번째 좌석을 예약하셨습니다.";
+			}
 		}
 		System.out.println(result);
 	}
-	
+
+	int recharge(int myMoney) {
+		System.out.println(" 충전 금액을 입력하세요.");
+		addMoney = Util.sc.nextInt();
+		myMoney += addMoney;
+		System.out.println("보유금액: " + myMoney);
+       return myMoney;
+	}
 
 }
